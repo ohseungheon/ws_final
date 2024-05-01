5af814eca70ae96c8c7c20699558d104baf6d8a5
quswldms137 <ster137@naver.com>
2024-05-01 15:58:10 +0900

    컨트롤러++

---
+++ demo_final/src/main/java/com/example/demo_final/dao/IUserdao.java
@@ -0,0 +1,15 @@
+package com.example.demo_final.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo_final.dto.UserDto;
+
+@Mapper
+public interface IUserdao {
+	public UserDto getUserInfo(String uno); 
+	public List<UserDto> getUserInfoList();
+	public int saveUserInfo(UserDto dto );
+	public int deleteUserInfo(String uno);
+}
